7bc7e83f3a0194957669506330eb5ad60de03b42
Discookie <matekos17@fazekas.hu>
2020-10-29 15:12:04 +0100

    client: remove extra print statements

--- src/client.py
+++ src/client.py
@@ -7,10 +7,6 @@ with socket()as main_socket:
  numbers=range(1,101)
  while numbers:
   length=len(numbers)>>1
-  print(numbers[length])
   main_socket.send(pack("is",numbers[length],{0:b"="}.get(length,b"<")))
-  time.sleep(1)
-  x=main_socket.recv(1);print(x)
-  numbers={b"I":numbers[:length],b"N":numbers[length:]}.get(x,0)
-  print(numbers)
+  numbers={b"I":numbers[:length],b"N":numbers[length:]}.get(main_socket.recv(1),0)
  main_socket.close()
