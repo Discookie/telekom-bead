97a97e188f94e4b034ce3ae30eae7447975cf9b7
Discookie <matekos17@fazekas.hu>
2020-10-30 01:11:49 +0100

    server: 369 chars using continue

--- src/server.py
+++ src/server.py
@@ -8,14 +8,12 @@ with socket()as main_socket:
  while 1:
   readable=select(socket_list,(),())[0]
   for new_socket in readable:
-   if socket_list[0]==new_socket:socket_list+=new_socket.accept()[:1];guess_number=guess_number if socket_list[2:]else randint(1,100)
-   else:
-    new_recv=new_socket.recv(5)
-    if new_recv:
-     received_num,received_char=unpack("is",new_recv)
-     if received_char==b"=":
-      if received_num-guess_number:new_socket.send(b"K");socket_list.remove(new_socket)
-      else:
-       for socket_item in socket_list[1:]:socket_item.send(b"Y"if socket_item==new_socket else b"V");socket_list.remove(socket_item)
-     else:new_socket.send(b"N"if("<"==received_char)==(guess_number<received_num)else b"I")
-    else:socket_list.remove(new_socket)
+   if socket_list[0]==new_socket:socket_list+=new_socket.accept()[:1];guess_number=guess_number if socket_list[2:]else randint(1,100);continue
+   new_recv=new_socket.recv(5)
+   if new_recv:
+    received_num,received_char=unpack("is",new_recv)
+    if received_char==b"=":
+     if received_num-guess_number:new_socket.send(b"K");socket_list.remove(new_socket);continue
+     for socket_item in socket_list[1:]:socket_item.send(b"Y"if socket_item==new_socket else b"V");socket_list.remove(socket_item)
+    else:new_socket.send(b"N"if("<"==received_char)==(guess_number<received_num)else b"I")
+   else:socket_list.remove(new_socket)
