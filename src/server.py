e6e72a30f125d6ad1717eaec9ca6a06dc1949693
Discookie <matekos17@fazekas.hu>
2020-10-30 01:08:20 +0100

    server: 372 chars

--- src/server.py
+++ src/server.py
@@ -4,14 +4,11 @@ from random import*
 from select import*
 from sys import*
 with socket()as main_socket:
- main_socket.bind((argv[1],int(argv[-1])))
- main_socket.listen()
- socket_list=[main_socket]
+ main_socket.bind((argv[1],int(argv[-1])));main_socket.listen();socket_list=[main_socket]
  while 1:
   readable=select(socket_list,(),())[0]
   for new_socket in readable:
-   if socket_list[0]==new_socket:
-    socket_list+=new_socket.accept()[:1];guess_number=guess_number if socket_list[2:]else randint(1,100)
+   if socket_list[0]==new_socket:socket_list+=new_socket.accept()[:1];guess_number=guess_number if socket_list[2:]else randint(1,100)
    else:
     new_recv=new_socket.recv(5)
     if new_recv:
