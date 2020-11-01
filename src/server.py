db9ab447ec0bb9f5659efdab814fd8582ddf1571
Discookie <matekos17@fazekas.hu>
2020-11-01 03:24:50 +0100

    server: 360, remove only minus

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 from socket import*;from random import*;from select import*;from sys import*
 with socket()as main_socket:
- main_socket.bind((argv[1],int(argv[-1])));main_socket.listen();socket_list=[main_socket]
+ main_socket.bind((argv[1],int(argv[2])));main_socket.listen();socket_list=[main_socket]
  while socket_list:
   readable=select(socket_list,socket_list,socket_list)[0]
   for new_socket in readable:
@@ -12,6 +12,6 @@ with socket()as main_socket:
     socket_list.remove(new_socket);new_recv[0]=b"KY"[new_recv[1]==guess_number]
     if new_recv[1]==guess_number:
      for socket_item in socket_list[1:]:socket_item.send(b"V]==b");socket_list.remove(socket_item)
-   elif new_recv:new_recv[0]=b"NI"[(new_recv[:1]==b"<")==(guess_number<new_recv[1])]
+   elif new_recv:new_recv[0]=b"NI"[(new_recv[:1]==b">")==(new_recv[1]>guess_number)]
    else:socket_list.remove(new_socket)
    new_socket.send(new_recv)
