cecbfa7a34b04805c2c8efdc89edbd1f1f1a56ee
Discookie <matekos17@fazekas.hu>
2020-11-01 04:51:53 +0100

    client: 215 using long zero pad

--- src/client.py
+++ src/client.py
@@ -1,5 +1,5 @@
 from socket import*;from sys import*
 with socket()as main_socket:
  main_socket.connect((argv[1],int(argv[-1])));numbers=range(1,101)
- while numbers:length=len(numbers)>>1;main_socket.send(bytes([61-(length>0),numbers[length]]+[0]*3));numbers={73:numbers[:length],78:numbers[length:]}.get(main_socket.recv(5)[0],0)
+ while numbers:length=len(numbers)>>1;main_socket.send(bytes([61-(length>0),numbers[length],0,0,0]));numbers={73:numbers[:length],78:numbers[length:]}.get(main_socket.recv(5)[0],0)
  main_socket.close()
