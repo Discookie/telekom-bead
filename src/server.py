05cf295a8cfe4f5c0406c998a0335705367dd9dc
Discookie <matekos17@fazekas.hu>
2020-11-01 03:29:11 +0100

    server: 357 remove semicolons

--- src/server.py
+++ src/server.py
@@ -1,17 +1,25 @@
-from socket import*;from random import*;from select import*;from sys import*
+from socket import*
+from random import*
+from select import*
+from sys import*
 with socket()as main_socket:
- main_socket.bind((argv[1],int(argv[2])));main_socket.listen();socket_list=[main_socket]
+ main_socket.bind((argv[1],int(argv[2])))
+ main_socket.listen()
+ socket_list=[main_socket]
  while socket_list:
   readable=select(socket_list,socket_list,socket_list)[0]
   for new_socket in readable:
    if socket_list[0]==new_socket:
     if socket_list[:1]==socket_list:guess_number=randint(1,100)
-    socket_list+=new_socket.accept()[:1];continue
+    socket_list+=new_socket.accept()[:1]
+    continue
    new_recv=bytearray(new_socket.recv(5))
    if new_recv[:1]==b"=":
-    socket_list.remove(new_socket);new_recv[0]=b"KY"[new_recv[1]==guess_number]
+    socket_list.remove(new_socket)
+    new_recv[0]=b"KY"[new_recv[1]==guess_number]
     if new_recv[1]==guess_number:
-     for socket_item in socket_list[1:]:socket_item.send(b"V]==b");socket_list.remove(socket_item)
+     for socket_item in socket_list[1:]:socket_item.send(b"V]==b")
+     socket_list=socket_list[:1]
    elif new_recv:new_recv[0]=b"NI"[(new_recv[:1]==b">")==(new_recv[1]>guess_number)]
    else:socket_list.remove(new_socket)
    new_socket.send(new_recv)
