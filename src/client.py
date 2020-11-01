a6ae559aa432df2994579bf3fa9a5ce4e9d01308
Discookie <matekos17@fazekas.hu>
2020-11-01 04:45:12 +0100

    client: 225 one-line everything

--- src/client.py
+++ src/client.py
@@ -1,10 +1,5 @@
-from socket import*
-from sys import*
+from socket import*;from sys import*
 with socket()as main_socket:
- main_socket.connect((argv[1],int(argv[-1])))
- numbers=range(1,101)
- while numbers:
-  length=len(numbers)>>1
-  main_socket.send(bytes([61-(length>0),numbers[length]]+[0]*3))
-  numbers={b"I":numbers[:length],b"N":numbers[length:]}.get(main_socket.recv(5)[:1],0)
+ main_socket.connect((argv[1],int(argv[-1])));numbers=range(1,101)
+ while numbers:length=len(numbers)>>1;main_socket.send(bytes([61-(length>0),numbers[length]]+[0]*3));numbers={b"I":numbers[:length],b"N":numbers[length:]}.get(main_socket.recv(5)[:1],0)
  main_socket.close()
