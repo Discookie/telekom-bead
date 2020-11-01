de8b54b6092230bcf9c7ec955cb0b3e496a716fc
Discookie <matekos17@fazekas.hu>
2020-11-01 04:44:54 +0100

    client: 230 reworked sending

--- src/client.py
+++ src/client.py
@@ -5,6 +5,6 @@ with socket()as main_socket:
  numbers=range(1,101)
  while numbers:
   length=len(numbers)>>1
-  main_socket.send({0:b"="}.get(length,b"<")+bytes((numbers[length],0,0,0)))
+  main_socket.send(bytes([61-(length>0),numbers[length]]+[0]*3))
   numbers={b"I":numbers[:length],b"N":numbers[length:]}.get(main_socket.recv(5)[:1],0)
  main_socket.close()
