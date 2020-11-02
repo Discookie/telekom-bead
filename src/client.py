3b7cdaaffe1e080cbfc785c4fca6e0cffa83e3bf
Discookie <matekos17@fazekas.hu>
2020-11-02 23:08:41 +0100

    Temporary client debug for submission

--- src/client.py
+++ src/client.py
@@ -1,5 +1,5 @@
 import sys,socket
 with socket.socket()as main_socket:
  main_socket.connect((sys.argv[1],int(sys.argv[-1])));numbers=range(1,101)
- while numbers:main_socket.send(bytes((61-(len(numbers)>1),numbers[len(numbers)>>1],0,0,0)));numbers={b"N":numbers[len(numbers)>>1:],b"I":numbers[:len(numbers)>>1]}.get(main_socket.recv(5)[:1],0)
+ while numbers:print((61-(len(numbers)>1),numbers[len(numbers)>>1],0,0,0));main_socket.send(bytes((61-(len(numbers)>1),numbers[len(numbers)>>1],0,0,0)));numbers={b"N":numbers[len(numbers)>>1:],b"I":numbers[:len(numbers)>>1]}.get((x:=main_socket.recv(5),print(x),x[:1])[2],0)
  main_socket.close()
