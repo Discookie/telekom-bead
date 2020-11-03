a5b3cfc92088350e4796a6fb9433caf77f824153
Discookie <matekos17@fazekas.hu>
2020-11-03 19:52:24 +0100

    server: Regression 358, proper length msgs

--- src/server.py
+++ src/server.py
@@ -10,13 +10,13 @@ with socket.socket()as main_socket:
     if socket_list[:1]==socket_list:guess_number=random.randint(1,100)
     socket_list+=[new_socket.accept()[0]]
    else:
-    new_recv=list(new_socket.recv(5))
+    new_recv=list(new_socket.recv(8))
     if new_recv[:1]==[61]:
-     new_recv[0]=b"KY"[new_recv[-1]==guess_number]
+     new_recv[0]=b"KY"[new_recv[4]==guess_number]
      socket_list.remove(new_socket)
-     if new_recv[-1]==guess_number:
-      for socket_item in socket_list[1:]:socket_item.send(b"V0]=b")
+     if new_recv[4]==guess_number:
+      for socket_item in socket_list[1:]:socket_item.send(b"V      b")
       socket_list[1:]=()
-    elif new_recv:new_recv[0]=b"NI"[[new_recv[-1]<guess_number,new_recv[-1]>guess_number][new_recv[0]==60]]
+    elif new_recv:new_recv[0]=b"NI"[[new_recv[4]<guess_number,new_recv[4]>guess_number][new_recv[0]==60]]
     else:socket_list.remove(new_socket)
     new_socket.send(bytes(new_recv))
