5238b99dbeb11032c40e49e53b9ed6a29a99ee87
Discookie <matekos17@fazekas.hu>
2020-11-04 02:26:53 +0100

    server: Fix swapped recv values

--- src/server.py
+++ src/server.py
@@ -11,8 +11,8 @@ with socket.socket()as main_socket:
     socket_list+=[new_socket.accept()[0]]
    elif new_recv:=list(new_socket.recv(8)):
     if new_recv[0]==61:socket_list.remove(new_socket);new_recv[0]=b"KY"[new_recv[4]==guess_number]
-    if new_recv[0]==60:new_recv[0]=b"NI"[new_recv[4]<guess_number]
-    if new_recv[0]==62:new_recv[0]=b"NI"[new_recv[4]>guess_number]
+    if new_recv[0]==60:new_recv[0]=b"NI"[new_recv[4]>guess_number]
+    if new_recv[0]==62:new_recv[0]=b"NI"[new_recv[4]<guess_number]
     if new_recv[0]==89:
      for socket_item in socket_list[1:]:socket_item.send(b"V new_recv[0]=b")
      socket_list[1:]=()
