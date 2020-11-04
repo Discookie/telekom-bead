231f640a21947427b8a8002d26971b1d92e35b79
Discookie <matekos17@fazekas.hu>
2020-11-04 01:58:23 +0100

    server: 347 better compressing if's

--- src/server.py
+++ src/server.py
@@ -9,14 +9,12 @@ with socket.socket()as main_socket:
    if socket_list[0]==new_socket:
     if socket_list[:1]==socket_list:guess_number=random.randint(1,100)
     socket_list+=[new_socket.accept()[0]]
-   else:
-    new_recv=list(new_socket.recv(8))
-    if new_recv[:1]==[61]:
-     new_recv[0]=b"KY"[new_recv[4]==guess_number]
-     socket_list.remove(new_socket)
-     if new_recv[4]==guess_number:
-      for socket_item in socket_list[1:]:socket_item.send(b"V new_recv[0]=b")
-      socket_list[1:]=()
-    elif new_recv:new_recv[0]=b"NI"[[new_recv[4]<guess_number,new_recv[4]>guess_number][new_recv[0]==60]]
-    else:socket_list.remove(new_socket)
+   elif new_recv:=list(new_socket.recv(8)):
+    if new_recv[0]==61:socket_list.remove(new_socket);new_recv[0]=b"KY"[new_recv[4]==guess_number]
+    if new_recv[0]==60:new_recv[0]=b"NI"[new_recv[4]<guess_number]
+    if new_recv[0]==62:new_recv[0]=b"NI"[new_recv[4]>guess_number]
+    if new_recv[0]==89:
+     for socket_item in socket_list[1:]:socket_item.send(b"V new_recv[0]=b")
+     socket_list[1:]=()
     new_socket.send(bytes(new_recv))
+   else:socket_list.remove(new_socket)
