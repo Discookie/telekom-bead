020906777948e1f7fbadc91d7c81168ed65810b2
Discookie <matekos17@fazekas.hu>
2020-11-08 20:44:39 +0100

    chk: 277 negative idx

--- src/checksum_srv.py
+++ src/checksum_srv.py
@@ -4,6 +4,6 @@ with socket.socket()as main_socket:
  while new_socket:=main_socket.accept()[0]:
   while new_recv:=new_socket.recv(100):
    split_string=new_recv.split(b"|")
-   if split_string[0]==b"BE":checksum_list[split_string[1]]=[time.time()+int(split_string[2]),split_string[3]+b"|"+split_string[4]];new_socket.send(b"OK")
+   if split_string[0]==b"BE":checksum_list[split_string[1]]=(time.time()+int(split_string[2]),split_string[-2]+b"|"+split_string[-1]);new_socket.send(b"OK")
    elif time.time()<checksum_list.get(split_string[1],[0])[0]:new_socket.send(checksum_list[split_string[1]][1])
    else:new_socket.send(b"0|")
