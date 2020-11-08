7a2b5c54e68e92b98a908c6d7834a83134dad3b8
Discookie <matekos17@fazekas.hu>
2020-11-08 22:00:19 +0100

    cli: 272 more migration

--- src/netcopy_cli.py
+++ src/netcopy_cli.py
@@ -1,6 +1,5 @@
 import socket,hashlib,sys
 with socket.socket()as cli_socket:
  in_file,md5_processor=open(sys.argv[-1],"rb"),hashlib.md5();cli_socket.connect((sys.argv[1],int(sys.argv[2])))
- while new_recv:=in_file.read(1000):cli_socket.send(new_recv);md5_processor.update(new_recv)
- with socket.socket()as checksum_socket:checksum_socket.connect((sys.argv[3],int(sys.argv[-3])));checksum_socket.send(b"|".join((b"BE",sys.argv[-2].encode(),b"5|32",md5_processor.hexdigest().encode())));checksum_socket.recv(3);checksum_socket.close()
- cli_socket.close()
+ while cli_socket.send(new_recv:=in_file.read(1000)):md5_processor.update(new_recv)
+ with socket.socket()as checksum_socket:checksum_socket.connect((sys.argv[3],int(sys.argv[-3])));checksum_socket.send(b"|".join((b"BE",sys.argv[-2].encode(),b"5|32",md5_processor.hexdigest().encode())));checksum_socket.recv(3);checksum_socket.close();cli_socket.close()
