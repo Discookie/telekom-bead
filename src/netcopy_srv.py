18c509bea89583bcdd1916e09c3ca87eb791e7d9
Discookie <matekos17@fazekas.hu>
2020-11-08 21:42:34 +0100

    srv: 315 swap == order

--- src/netcopy_srv.py
+++ src/netcopy_srv.py
@@ -2,4 +2,4 @@ import socket,hashlib,sys
 with socket.socket()as main_socket:
  out_file,md5_processor=open(sys.argv[5],"wb"),hashlib.md5();main_socket.bind((sys.argv[1],int(sys.argv[2])));main_socket.listen();cli_socket=main_socket.accept()[0]
  while out_file.write(new_recv:=cli_socket.recv(1000)):md5_processor.update(new_recv)
-with socket.socket()as checksum_socket:checksum_socket.connect((sys.argv[3],int(sys.argv[-3])));checksum_socket.send(b"KI|"+sys.argv[-2].encode());print("CSUM","OK"if checksum_socket.recv(1000)[3:]==md5_processor.hexdigest().encode()else"CORRUPTED");checksum_socket.close()
+with socket.socket()as checksum_socket:checksum_socket.connect((sys.argv[3],int(sys.argv[-3])));checksum_socket.send(b"KI|"+sys.argv[-2].encode());print("CSUM","OK"if md5_processor.hexdigest().encode()==checksum_socket.recv(1000)[3:]else"CORRUPTED");checksum_socket.close()
