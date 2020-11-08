2cb37dbf1c787988e53494da9e745a33bac60bd6
Discookie <matekos17@fazekas.hu>
2020-11-08 21:30:17 +0100

    srv: 318 different file open method

--- src/netcopy_srv.py
+++ src/netcopy_srv.py
@@ -1,6 +1,5 @@
 import socket,hashlib,sys
 with socket.socket()as main_socket:
- md5_processor=hashlib.md5();main_socket.bind((sys.argv[1],int(sys.argv[2])));main_socket.listen();cli_socket=main_socket.accept()[0]
- with open(sys.argv[-1],"wb")as out_file:
-  while out_file.write(new_recv:=cli_socket.recv(1000)):md5_processor.update(new_recv)
+ out_file=open(sys.argv[-1],"wb");md5_processor=hashlib.md5();main_socket.bind((sys.argv[1],int(sys.argv[2])));main_socket.listen();cli_socket=main_socket.accept()[0]
+ while out_file.write(new_recv:=cli_socket.recv(1000)):md5_processor.update(new_recv)
 with socket.socket()as checksum_socket:checksum_socket.connect((sys.argv[3],int(sys.argv[-3])));checksum_socket.send(b"KI|"+sys.argv[-2].encode());print("CSUM","OK"if checksum_socket.recv(1000)[3:]==md5_processor.hexdigest().encode()else"CORRUPTED");checksum_socket.close()
