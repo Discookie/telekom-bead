8ca12b38ba14f6baa77cebb1af714a71d23740b0
Discookie <matekos17@fazekas.hu>
2020-11-08 21:22:23 +0100

    srv: 323 different print

--- src/netcopy_srv.py
+++ src/netcopy_srv.py
@@ -3,8 +3,4 @@ with socket.socket()as main_socket:
  md5_processor=hashlib.md5();main_socket.bind((sys.argv[1],int(sys.argv[2])));main_socket.listen();cli_socket=main_socket.accept()[0]
  with open(sys.argv[-1],"wb")as out_file:
   while out_file.write(new_recv:=cli_socket.recv(1000)):md5_processor.update(new_recv)
-with socket.socket()as checksum_socket:
- checksum_socket.connect((sys.argv[3],int(sys.argv[-3])));checksum_socket.send(b"KI|"+sys.argv[-2].encode())
- if checksum_socket.recv(1000)[3:]==md5_processor.hexdigest().encode():print("CSUM OK")
- else:print("CSUM CORRUPTED")
- checksum_socket.close()
+with socket.socket()as checksum_socket:checksum_socket.connect((sys.argv[3],int(sys.argv[-3])));checksum_socket.send(b"KI|"+sys.argv[-2].encode());print("CSUM","OK"if checksum_socket.recv(1000)[3:]==md5_processor.hexdigest().encode()else"CORRUPTED");checksum_socket.close()
